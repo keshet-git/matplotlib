a8891ac400ec80210963ae97ae239160e36f88dc
keshet-git <83539396+keshet-git@users.noreply.github.com>
2023-01-10 23:54:16 +0200

    Add files via upload

---
+++ Multidimensional.1.py/index.py
@@ -0,0 +1,10 @@
+movies = {
+    'The Godphther': {'year': 1972, 'actors':['Al Pacino','James Caan']},
+    'Fight Club': {'year': 1999, 'actors':['Edwaid Norton','Brad Pitt']},
+    'The Matrix': {'year': 1999, 'actors':['Keanu Reeves', 'Hugo Weaving']},
+    'inception': {'year': 2010, 'actors':['Leonardo DeCaprio',' Ellen Page']},
+    'Seven': {'year': 1995, 'actors':['Brad Pitt','Morgan Freeman']},
+    'The devils Advocate': {'year': 1995, 'actors':['Keanu Reeves','Al Pacino']}
+}
+for key, value in movies.items():
+    print(key, value)
---
+++ multiple_figures.1.py/index.py
@@ -0,0 +1,10 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.linear_model import LinearRegression
+from sklearn.preprocessing import PolynomialFeatures
+
+x = np.array([1,2,3,2,1])
+y = np.array([1,3,2,3,1])
+
+plt(x, y)
+plt.show()
